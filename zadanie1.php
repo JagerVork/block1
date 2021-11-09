902b8cbad2a874d1b835e6872562e70d6dd744f3
JagerVork <93756905+JagerVork@users.noreply.github.com>
2021-11-09 23:51:41 +0700

    Update zadanie1.php

--- zadanie1.php
+++ zadanie1.php
@@ -1,18 +1,22 @@
 <?php
-$array_users = array(
+$array_users = array (
     "Иван Иванов",
     "Иван Петров",
     "Петр Иванов",
     "Петр Петров",
     "Сидор Иванов",
-    "Сидор Петров"
-);
-$massiveA= array('');
-$massiveB= array('');
-    if((array_search("Петр Петров", $array_users){
-        echo ($massiveA);
-    }else{
-        echo ($massiveB);
-    }
-
+    "Сидор Петров")
+;
+$subStr='Гена Букин';
+$result1 = array();
+$result2 = array();
+$search= strpos($array_users, $subStr);
+if ($pos === false){
+    array_unshift($result1, $subStr);
+    var_dump ($result1);
+}else{
+    array_unshift($result2, $subStr);
+    var_dump ($result2);
+}
+print_r ($array_users) ;
 ?>
