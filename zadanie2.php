84cbc9d2bafef1776d619c1d2e0638e4df7bd487
JagerVork <93756905+JagerVork@users.noreply.github.com>
2021-11-11 22:40:00 +0700

    Update zadanie2.php

--- zadanie2.php
+++ zadanie2.php
@@ -1,21 +1,28 @@
 <?php
-$cars=array('Acura','Audi','BMW','Bugatti','Buick','Cadillac','Chery','Citroen','Acura','BMW','Cadillac');
-$cars2=array_unique($cars);
+function test (&$array,$key1,$key2) 
+{
+    list($array[$key1],$array[$key2]) = array($array[$key2],$array[$key1]);
 
-print_r($videogame2);
+}
 
-
-rsort($cars2);
-print_r($cars2);
-echo "<br/>";
-
-
-$videogame=array('CS:GO','CoD:MW','BF5','WWZ','GTA','LostArk','R6','WWZ','CoD:MW','LostArk','WWZ');
-$videogame2=array_unique($videogame);
-
-print_r($cars2);
-echo "<br/>";
-
-arsort($videogame2);
-print_r($videogame2);
+function sortirovka ($s) 
+{
+    $i = $j = 0; 
+    if (is_array($s)) {
+        $n = count($s);
+    } else {
+        $s = (string)$s;
+        $n = mb_strlen($s);
+    }	
+    for ($i=1; $i<$n; $i++) {
+        $j = $i;
+        while (($j>0) && ($s[$j] < $s[$j-1])) {
+            test($s,$j,$j-1);
+            $j = $j-1;
+        }
+    }
+    return $s;
+}
+$s = array(7,13,66,0,49,91,234,777,49,65);
+var_dump(sortirovka($s));
 ?>
