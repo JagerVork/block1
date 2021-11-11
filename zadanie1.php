48d91825e848637ea55d09cd34f2467e6d6f2270
JagerVork <93756905+JagerVork@users.noreply.github.com>
2021-11-11 22:39:31 +0700

    Update zadanie1.php

--- zadanie1.php
+++ zadanie1.php
@@ -11,6 +11,14 @@ $subStr='Гена Букин';
 $result1 = array();
 $result2 = array();
 $search= strpos($array_users, $subStr);
+for ($i=0;$i<count($array_users); $i++)
+    for($j=0; $j<count($subStr); $j++)
+    if ($array_users[$i]==$subStr[$j]){
+     $result1[] = $array_users[$i];
+     $result2[] = $array_users[$j];
+    }
+    {
+
 if ($pos === false){
     array_unshift($result1, $subStr);
     var_dump ($result1);
@@ -18,5 +26,5 @@ if ($pos === false){
     array_unshift($result2, $subStr);
     var_dump ($result2);
 }
-print_r ($array_users) ;
+    }
 ?>
