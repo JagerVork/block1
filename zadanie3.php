52b4cdad5bfecd7360aa70779205aa66acd9632f
JagerVork <93756905+JagerVork@users.noreply.github.com>
2021-11-13 14:01:02 +0700

    Update zadanie3.php

--- zadanie3.php
+++ zadanie3.php
@@ -1,12 +1,25 @@
 <?php
-$str = 'qwertyuiopasdfghjklzxcvbnm';
-if ((strlen($str)) % 2==0 ) {
-    echo "Boy!";
-} 
-else  {
-    echo "Girl!";
+function random_nickname()
+{
+$strmas=array("q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m");
+$chislo=rand(10,100);
+$nickname='';
+for ($i=10; $i<$chislo; $i++){
+    $nickname .=$strmas[random_int(10,count($strmas)-1)];
 }
+return $nickname;
+}
+$nick = join(array_unique(preg_split("//u",random_nickname())));
+echo $nick;
 echo "<br/>";
-echo substr(str_shuffle($str),10,100);
-
+$str_nickname=mb_strlen($nick,'utf8');
+echo $str_nickname;
+echo "<br/>";
+{
+    if(($str_nickname % 2)==0) {
+        print ("Boy!");
+    }else{
+        print ("Girl!");
+    }
+}
 ?>
